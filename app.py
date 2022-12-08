1098a9e74ce7947384166856ff5bd36e4d9bc6b8
*S0nVard* <sona@uni.minerva.edu>
2022-12-08 19:30:28 -0300

    adding a major database change, functionality for the tracker and displaying the actions

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from ast import For
 from flask import Flask, redirect, render_template, request, session, url_for, redirect, flash
+from flask_session import Session
 from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -6,10 +8,11 @@ from flask_wtf import FlaskForm
 from flask_migrate import Migrate
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
 from wtforms import BooleanField, PasswordField, StringField
-from wtforms.validators import InputRequired, Length#, Email
+from wtforms.validators import InputRequired, Length #, Email
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from functools import wraps
+from sqlalchemy import ForeignKey
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -17,11 +20,17 @@ Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 #app.config['SQLAlchemy_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'BAD_SECRET_KEY'
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     username = db.Column(db.String(50), unique=True, nullable = False)
     email = db.Column(db.String(50), unique=True, nullable = False)
     password = db.Column(db.String(80), nullable = False)
@@ -30,13 +39,32 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return "<Name %r>" % self.username
 
+# add a column for username to retrieve the goals for the current user
 class Big_goal(db.Model):
-    id = db.Column(db.Integer, primary_key = True )
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True )
+    user_id = db.Column(db.Integer, ForeignKey(User.id))
     goal = db.Column(db.String(1000), nullable = False)
     date_added = db.Column(db.DateTime, default=datetime.now())
 
     def __repr__(self):
         return "<Goal %r>" % self.goal
+    
+class Habit(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True )
+    big_goal_id = db.Column(db.Integer, ForeignKey(Big_goal.id))
+    name = db.Column(db.String(200), nullable = False)
+    completion = db.Column(db.Integer, default=0)
+    
+class Action(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    habit_id = db.Column(db.Integer, ForeignKey(Habit.id))
+    week = db.Column(db.Integer)
+    day = db.Column(db.Integer)
+    name = db.Column(db.String(1000), nullable=False)
+    completed = db.Column(db.Boolean, unique=False, default=False)
+
+    def __repr__(self):
+        return '<Action %r>' % self.id
         
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -50,7 +78,7 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=80)])
     remember = BooleanField('Remember me')
-    
+  
     
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(), #Email(message='Invalid email'), 
@@ -61,12 +89,24 @@ class RegistrationForm(FlaskForm):
 class Goal(FlaskForm):
     Goal = StringField('goal', validators=[InputRequired(), Length(min=1, max=1000)])            
 
+class Tree(FlaskForm):
+    habit = StringField('habit', validators=[InputRequired(), Length(min=1, max=1000)])
+    action = StringField('action', validators=[InputRequired(), Length(min=1, max=10000)])
+
 with app.app_context():
     db.create_all()
     db.session.commit()
 
+# Create a decorator (helper function for @app.route) that requires the user to log in at specified pages
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("email") is None:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route('/homepage', methods=['GET','POST'])
-#@login_required
 def homepage():
     form=Goal()
     if request.method == "POST":
@@ -77,17 +117,16 @@ def homepage():
         
         db.session.add(new_goal)
         db.session.commit()
+        session['goal'] = Big_goal.query.order_by(Big_goal.date_added.desc()).first()
         print(f'submitted long-term goal: {goal}')
         return redirect(url_for('habits'))
     
     return render_template("homepage.html", form=form)
 
-@app.route('/signup')
-def signup():
-    return render_template("signup.html")
-
 @app.route('/', methods=['GET', 'POST'])
 def login():
+    #session.clear()
+    
     if current_user.is_authenticated: return(url_for('habits'))
     form = LoginForm()
     if form.validate_on_submit():
@@ -97,19 +136,29 @@ def login():
             remember = True if form.password.data else False
 
             user = User.query.filter_by(username=username).first()
-            print(user)
+            #print(user)
             # check if the user actually exists
             # take the user-supplied password, hash it, and compare it to the hashed password in the database
             if not user or not check_password_hash(user.password, password):
                 flash('Please check your login details and try again.')
                 #print('invalid data')
                 return redirect(url_for('login'))
-                
+            #adding sessions
+            session['id'] = user.id 
+            session["username"] = user.username
+            session["email"] = user.email
+            session['goal'] = Big_goal.query.order_by(Big_goal.date_added.desc()).first()
+            print("FROM SESSION:", session['username'])
             return redirect(url_for("habits"))
-    
+        
     return render_template("login.html", form=form)
 
-
+# route for loggin out the user
+@app.route("/logout")
+@login_required
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -117,7 +166,7 @@ def register():
     form = RegistrationForm() 
     #print('Created FORM')
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+        user = User.query.filter_by(username=form.username.data).first()
         #print('Queried USER') 
         if user is None:
             #print(form.username.data, form.email.data, form.password.data)
@@ -136,29 +185,84 @@ def register():
     return render_template("register.html", form=form)
 
 @app.route('/habits')
+@login_required
 def habits():
-    goal = Big_goal.query.order_by(Big_goal.date_added.desc()).first()
-    print(f"retrieved your goal {goal}")
-    return render_template("habits.html", goal=goal.goal)
+    
+    #goal = Big_goal.query.order_by(Big_goal.date_added.desc()).first()
+    goal = session['goal']
+    habits = Habit.query.filter_by(big_goal_id=goal.id).all()
+    for habit in habits:
+        actions = Action.query.filter_by(habit_id=habit.id).all()
+        if len(actions) > 0:
+            fraction = int(sum([1 if a.completed else 0 for a in actions])/len(actions)*100)
+            habit.completion = fraction
+            db.session.commit()
+        print(habit.name, habit.completion)
+        
+    print(f"retrieved your goal {goal.goal} - {goal.date_added}")
+    return render_template("habits.html", goal=goal.goal, habits=habits)
 
-@app.route('/tree')
-def summary():
-    goal = Big_goal.query.order_by(Big_goal.date_added.desc()).first()
+@app.route('/tree', methods = ['POST', 'GET'])
+@login_required
+def tree():
+    db_week_action = Action.query.order_by(Action.id).all()
+    print(db_week_action)
+    #goal = Big_goal.query.order_by(Big_goal.date_added.desc()).first()
+    goal = session['goal']
+    habits = Habit.query.filter_by(big_goal_id=goal.id).all()
     print(f"retrieved your goal {goal}")
-    return render_template("tree.html", goal=goal.goal)
+
+    return render_template("tree.html", goal=goal.goal, db_week_action=db_week_action, habits=habits)
+
+@app.route('/add_action/<habit_id>', methods=['POST', 'GET'])
+@login_required
+def add_action(habit_id):
+    if request.method == "POST":
+        action_name = request.form.get('name')
+        
+        new_action = Action(name = action_name,
+                            habit_id = habit_id,
+                            week=1,
+                            day=1,
+                            completed=False)
+
+        db.session.add(new_action)
+        db.session.commit()
+        return redirect(url_for("tree"))
+    else:
+        return redirect(url_for("tree"))
+
+
+@app.route('/add_habit', methods=['POST', 'GET'])
+@login_required
+def add_habit():
+    if request.method == "POST":
+
+        habit_name = request.form.get('habit')
+        new_habit = Habit(name = habit_name, big_goal_id=session["goal"].id)
+
+        db.session.add(new_habit)
+        db.session.commit()
+        return redirect(url_for("habits"))
+    else:
+        return redirect(url_for("habits"))
+
+@app.route('/change_status/<action_id>', methods=['POST', 'GET'])
+@login_required
+def change_status(action_id):
+    action = Action.query.filter_by(id=action_id).first()
+    action.completed = not action.completed
+    db.session.commit()
+    
+    return redirect(url_for("tree"))
+
 
 @app.route('/badges')
-def calendar():
+@login_required
+def badges():
     goal = Big_goal.query.order_by(Big_goal.date_added.desc()).first()
     print(f"retrieved your goal {goal}")
     return render_template("badges.html", goal=goal.goal)
-
-
-@app.route("/logout")
-def logout():
-    flash('Logged out successfully!', category='success')
-    logout_user()
-    return render_template('login.html')
 
 if __name__=="__main__":
     app.run(debug=True)
